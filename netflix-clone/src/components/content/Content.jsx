14b08218170a3b0549716f76980ecca21aab287a
kartik1502 <kartikkulkarni1411@gmail.com>
2022-11-17 20:59:54 +0530

    netflix-clone

---
+++ netflix-clone/src/components/content/Content.jsx
@@ -0,0 +1,12 @@
+
+import './Content.css'
+
+function Content() {
+  return (
+    <div className='content-wrapper'>
+
+    </div>
+  )
+}
+//8eff6f1c66dfa3283d878588d6ab6ff3
+export default Content;
