b4b757c74e7d8e6626fed292cccd0025aa0dfe9a
kartik1502 <kartikkulkarni1411@gmail.com>
2022-11-17 23:03:02 +0530

    netflix-clone

--- netflix-clone/src/components/content/Content.jsx
+++ netflix-clone/src/components/content/Content.jsx
@@ -1,12 +1,16 @@
 
 import './Content.css'
+import axios from 'axios'
 
 function Content() {
-  return (
-    <div className='content-wrapper'>
-
-    </div>
-  )
+  
+  const getMovies = () => {
+      const response = axios.get("https://api.themoviedb.org/3/movie/top_rated" ,{
+        params: {
+          api_key : "8eff6f1c66dfa3283d878588d6ab6ff3"
+        }
+      })
+  }
 }
-//8eff6f1c66dfa3283d878588d6ab6ff3
+//
 export default Content;
